71d4fb4a344eed8cc3673ef838cebae5c5f96839
leeya018 <leeyahav018@gmail.com>
2024-09-04 12:32:57 +0300

    game is almost ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,69 @@
 
 import RandomTaskSelector from "@/components/RandomTaskSelector";
 import RandomUserSelector from "@/components/RandomUserSelector";
+import { Task } from "@/interfaces/Task";
+import { User } from "@/interfaces/User";
+import { tasks, users } from "@/util";
 import React, { useState } from "react";
 
 export default function MainPage() {
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [isScrolling, setIsScrolling] = useState<boolean>(false);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isScrolling1, setIsScrolling1] = useState<boolean>(false);
+
+  const selectRandomTask = () => {
+    setIsScrolling(true);
+    let scrollIndex = 0;
+
+    const interval = setInterval(() => {
+      scrollIndex = (scrollIndex + 1) % tasks.length;
+      setSelectedTask(tasks[scrollIndex]);
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      const randomIndex = Math.floor(Math.random() * (tasks.length - 1)) + 1;
+      setSelectedTask(tasks[randomIndex]);
+      setIsScrolling(false);
+    }, 2000);
+  };
+
+  const selectRandomUser = () => {
+    setIsScrolling1(true);
+    let scrollIndex = 0;
+
+    const interval = setInterval(() => {
+      scrollIndex = (scrollIndex + 1) % users.length;
+      setSelectedUser(users[scrollIndex]);
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      const randomIndex = Math.floor(Math.random() * (users.length - 1)) + 1;
+      setSelectedUser(users[randomIndex]);
+      setIsScrolling1(false);
+    }, 3000);
+  };
+
   return (
-    <div>
-      <RandomUserSelector />
-      <RandomTaskSelector />
+    <div className="h-screen w-screen flex flex-col">
+      <div className="h-[30vh] flex justify-center items-center flex-col">
+        <RandomUserSelector selectedUser={selectedUser} />
+        <RandomTaskSelector selectedTask={selectedTask} />
+      </div>
+      <div className="flex-1 flex justify-center items-center border-2">
+        <button
+          onClick={() => {
+            selectRandomUser();
+            selectRandomTask();
+          }}
+          className="bg-green-500 text-white px-12 py-6 text-xl rounded hover:bg-green-600"
+          disabled={isScrolling}
+        >
+          Select Random
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/RandomTaskSelector.tsx
+++ src/components/RandomTaskSelector.tsx
@@ -2,39 +2,16 @@ import { Task } from "@/interfaces/Task";
 import { tasks } from "@/util";
 import { useState } from "react";
 
-const RandomTaskSelector: React.FC = () => {
-  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
-  const [isScrolling, setIsScrolling] = useState<boolean>(false);
+type RandomTaskSelector = {
+  selectedTask: Task | null;
+};
 
-  const selectRandomTask = () => {
-    setIsScrolling(true);
-    let scrollIndex = 0;
-
-    const interval = setInterval(() => {
-      scrollIndex = (scrollIndex + 1) % tasks.length;
-      setSelectedTask(tasks[scrollIndex]);
-    }, 100);
-
-    setTimeout(() => {
-      clearInterval(interval);
-      const randomIndex = Math.floor(Math.random() * (tasks.length - 1)) + 1;
-      setSelectedTask(tasks[randomIndex]);
-      setIsScrolling(false);
-    }, 2000);
-  };
-
+const RandomTaskSelector: React.FC<RandomTaskSelector> = ({ selectedTask }) => {
   return (
     <div className="text-center">
       <h1 className="text-2xl font-bold mb-4">
         {selectedTask ? selectedTask.title : "No task selected"}
       </h1>
-      <button
-        onClick={selectRandomTask}
-        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-        disabled={isScrolling}
-      >
-        Select Random Task
-      </button>
     </div>
   );
 };
